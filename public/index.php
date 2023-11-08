11f9ce3983e541bf3507780572210c34c28f53c8
Antonius Copeland <86853998+letyoursoulglo@users.noreply.github.com>
2023-11-08 14:47:05 -0500

    commented out functions file

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
 
-echo "<pre>";
-print_r($_SERVER);
-echo "<pre>";
+// include __DIR__ . "/../src/App/functions.php";
+
+// echo "<pre>";
+// print_r($_SERVER);
+// echo "</pre>";
 
 $app = include __DIR__ . '/../src/App/bootstrap.php';
 
 $app->run();
 
+// dd($app);
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -1,6 +1,6 @@
 <?php
 
-declar(strict_types=1);
+declare(strict_types=1);
 
 namespace Framework;
 
